87a27ce8fcfd1993f9029d9db32442900362ad26
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-27 19:19:26 +0100

    Update src/s3/s3_parameters.py

--- src/s3/s3_parameters.py
+++ src/s3/s3_parameters.py
@@ -51,10 +51,12 @@ class S3Parameters:
 
         # Parsing variables
         region_name = self.__secret.exc(secret_id='RegionCodeDefault')
-        internal = self.__secret.exc(secret_id='', node='internal')
+        internal = self.__secret.exc(secret_id='SustainabilityProject', node='internal')
+        external = self.__secret.exc(secret_id='SustainabilityProject', node='external')
 
         s3_parameters: s3p.S3Parameters = s3_parameters._replace(
-            location_constraint=region_name, region_name=region_name, internal=internal)
+            location_constraint=region_name, region_name=region_name,
+            internal=internal, external=external)
 
         return s3_parameters
 
