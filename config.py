8bd0ed25970f80aea3582d62e83f2974dd80f736
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-28 19:47:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,12 @@ class Config:
         Constructor
         """
 
+        # Temporary local areas
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.raw_ = os.path.join(self.warehouse, 'raw')
+        self.points_ = os.path.join(self.warehouse, 'points')
 
         # A S3 parameters template
         self.s3_parameters_template = 'https://raw.githubusercontent.com/preliminaries/.github/master/profile/s3_parameters.yaml'
 
-        self.s3_prefix = 'mileage/'
+        self.s3_internal_prefix = 'mileage/'
