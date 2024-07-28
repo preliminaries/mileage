99539401281150685bc25170f908a76e2e97bb07
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-28 21:15:00 +0100

    Update src/data/organisations.py

--- src/data/organisations.py
+++ src/data/organisations.py
@@ -1,3 +1,4 @@
+"""Module organisation.py"""
 import logging
 
 import pandas as pd
@@ -9,8 +10,17 @@ import src.elements.s3_parameters as s3p
 
 
 class Organisations:
+    """
+    Class Organisations
+    """
 
     def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+        """
+
+        :param service: A suite of services for interacting with Amazon Web Services.
+        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters settings of this
+                              project, e.g., region code name, buckets, etc.
+        """
 
         self.__service = service
         self.__s3_parameters = s3_parameters
@@ -33,5 +43,9 @@ class Organisations:
             filename='organisations.csv')
 
     def exc(self):
+        """
+
+        :return:
+        """
 
         return self.__organisations()
