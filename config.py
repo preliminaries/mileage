a784492af8f11075612f8d2d1210fc4531de2abd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-01 11:58:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -22,3 +22,11 @@ class Config:
         self.s3_parameters_template = 'https://raw.githubusercontent.com/preliminaries/.github/master/profile/s3_parameters.yaml'
 
         self.s3_internal_prefix = 'mileage/'
+
+        self.__rename = {'Area Pay Division': 'area_pay_division', 'Claim Line Start': 'claim_line_start',
+                         'Claim Line End': 'claim_line_end', 'Engine Size': 'engine_size',
+                         'Fuel Type': 'fuel_type', 'CO2 Emissions': 'co2_emissions',
+                         'Business Mileage': 'business_mileage', 'Business Rate High': 'business_rate_high',
+                         'Business Rate Low': 'business_rate_low', 'Business Value': 'business_value',
+                         'Commute Miles Not Undertaken': 'commute_miles_not_undertaken',
+                         'Overtime Mileage': 'overtime_mileage', 'Journey Details': 'journey_details'}
