a0a21a2e21b1b5b1d0c08665f543c946379452d5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-01 19:17:39 +0100

    Add src/data/prepare.py

---
+++ src/data/prepare.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+
+import config
+
+
+class Prepare:
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+    def exc(self, blob: pd.DataFrame, organisation_id: int) -> pd.DataFrame:
+        """
+
+        :param blob: A dataframe of mileage data
+        :param organisation_id: The identification code of the organisation whence the data came from
+        :return:
+        """
+
+        frame = blob.copy()
+
+        # Foremost, rename the fields; in line with naming conventions.
+        frame = frame.copy().rename(columns=self.__configurations.rename)
+
+        # Next, append a source organisation identification code.
+        frame = frame.assign(organisation_id=organisation_id)
+
+        return frame
