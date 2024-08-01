70d86b3a0b6c1984ad0285293cc97d88e011d36e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-01 18:20:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,13 +20,22 @@ class Config:
 
         # A S3 parameters template
         self.s3_parameters_template = 'https://raw.githubusercontent.com/preliminaries/.github/master/profile/s3_parameters.yaml'
-
         self.s3_internal_prefix = 'mileage/'
 
-        self.__rename = {'Area Pay Division': 'area_pay_division', 'Claim Line Start': 'claim_line_start',
-                         'Claim Line End': 'claim_line_end', 'Engine Size': 'engine_size',
-                         'Fuel Type': 'fuel_type', 'CO2 Emissions': 'co2_emissions',
-                         'Business Mileage': 'business_mileage', 'Business Rate High': 'business_rate_high',
-                         'Business Rate Low': 'business_rate_low', 'Business Value': 'business_value',
-                         'Commute Miles Not Undertaken': 'commute_miles_not_undertaken',
-                         'Overtime Mileage': 'overtime_mileage', 'Journey Details': 'journey_details'}
+        # Spreadsheets & Fields
+        self.parse_dates = ['Claim Line Start', 'Claim Line End']
+
+        self.rename = {'Area Pay Division': 'area_pay_division', 'Claim Line Start': 'claim_line_start',
+                       'Claim Line End': 'claim_line_end', 'Engine Size': 'engine_size',
+                       'Fuel Type': 'fuel_type', 'CO2 Emissions': 'co2_emissions',
+                       'Business Mileage': 'business_mileage', 'Business Rate High': 'business_rate_high',
+                       'Business Rate Low': 'business_rate_low', 'Business Value': 'business_value',
+                       'Commute Miles Not Undertaken': 'commute_miles_not_undertaken',
+                       'Overtime Mileage': 'overtime_mileage', 'Journey Details': 'journey_details'}
+
+        self.dtype = {'Area Pay Division': str, 'Engine Size': float,
+                      'Fuel Type': str, 'CO2 Emissions': float,
+                      'Business Mileage': float, 'Business Rate High': float,
+                      'Business Rate Low': float, 'Business Value': float,
+                      'Commute Miles Not Undertaken': float,
+                      'Overtime Mileage': float, 'Journey Details': str}
