07dedbe1c703019c14d536671d5ecbe6ebde06b7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-02 19:26:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,6 @@ class Config:
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.raw_ = os.path.join(self.warehouse, 'raw')
         self.initial_ = os.path.join(self.warehouse, 'initial')
-        self.points_ = os.path.join(self.warehouse, 'points')
 
         # A S3 parameters template
         self.s3_parameters_template = 'https://raw.githubusercontent.com/preliminaries/.github/master/profile/s3_parameters.yaml'
